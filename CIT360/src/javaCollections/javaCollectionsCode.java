bf79cfae0ddf5ed8f1534d7630a673d5c0293957
daltonsolo <daltonsolo@daltonsolo-PC>
2017-04-29 16:27:46 -0600

    Created javaCollections package

---
+++ CIT360/src/javaCollections/javaCollectionsCode.java
@@ -0,0 +1,11 @@
+package javaCollections;
+
+public class javaCollectionsCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int myNumber;
+		String myString;
+	}
+
+}
