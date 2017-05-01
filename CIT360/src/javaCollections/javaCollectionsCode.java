58210a973f7539945be1fb88281aec7ebd28cda7
daltonsolo <salty-dalty@hotmail.com>
2017-04-30 23:36:37 -0600

    Created some lists, added them to arrays, print

--- CIT360/src/javaCollections/javaCollectionsCode.java
+++ CIT360/src/javaCollections/javaCollectionsCode.java
@@ -1,14 +1,26 @@
 package javaCollections;
+import	java.util.*;
+import static java.util.Collections.*;
+
 
 public class javaCollectionsCode {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		/*
-Let's see if pushing changes works. I have cloned my repository so I can use Intellij instead of Eclipse.
-		 */
-		int myNumber;
-		String myString;
+		String[] stuff = {"strawberries", "blueberries", "blackberries", "raspberries"};
+		List<String> list1 = new ArrayList<>();
+
+		// Put array items into the list
+		addAll(list1, stuff);
+
+		String[] moreStuff = {"apples", "oranges", "strawberries", "blueberries"};
+		List<String> list2 = new ArrayList<>();
+
+		addAll(list2, moreStuff);
+
+		// Print the results of the array.
+        for(int i = 0;i<list2.size();i++) {
+            System.out.printf("%s ", list2.get(i));
+        }
 	}
 
 }
