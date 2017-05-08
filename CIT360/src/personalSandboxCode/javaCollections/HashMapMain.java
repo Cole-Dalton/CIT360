5cf0874b49c9a9e516d347f2b6fe46a06cfbee8c
daltonsolo <salty-dalty@hotmail.com>
2017-05-07 19:27:01 -0600

    Added TreeMap examples

--- CIT360/src/personalSandboxCode/javaCollections/HashMapMain.java
+++ CIT360/src/personalSandboxCode/javaCollections/HashMapMain.java
@@ -6,8 +6,10 @@ import java.util.*;
  * Created by daltonsolo on 5/6/2017.
  */
 public class HashMapMain {
-    // Create the HashMap with a string
+    // Create the HashMap with String values and keys
     static HashMap<String, String> departmentMap = new HashMap<>();
+    // Create TreeMap using Integer key and String value
+    static TreeMap<Integer, String> favoriteFoods = new TreeMap<Integer, String>();
     public static void main(String[] args) {
         PrintStream p = System.out;
         HashMapPerson p1 = new HashMapPerson("Dalton", "Cole", "Tech Support");
@@ -24,5 +26,24 @@ public class HashMapMain {
         p.println(departmentMap);
         // Can use specific keys to get the value like so:
         p.println("---------------------------------\nWho is in IT? ----> " + departmentMap.get("IT"));
+
+        // TreeMap examples
+
+        // Add stuff to the map. The difference between a TreeMap and a HashMap is that a TreeMap is ordered.
+        favoriteFoods.put(1, "Pizza");
+        favoriteFoods.put(2, "Spaghetti");
+        favoriteFoods.put(3, "Peanut Soup");
+        favoriteFoods.put(4, "Orange Chicken");
+        favoriteFoods.put(5, "Potatoes");
+        p.println("----------------------------------------------------------\nTreeMap\n----------------------------------------------------------\n");
+        p.println(favoriteFoods);
+        // To print my first favorite food, I'll use the firstKey() method.
+        p.println("My all time favorite food is " + favoriteFoods.get(favoriteFoods.firstKey()));
+        // To print the last one, I'll use lastKey().
+        p.println("My least favorite food is " + favoriteFoods.get(favoriteFoods.lastKey()));
+        // I can also print the keys in descending order:
+        p.println(favoriteFoods.descendingMap());
+
+
     }
 }
