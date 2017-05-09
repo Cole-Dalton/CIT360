ec77fa78e1ba2d61988e95bb8190697b08de1801
daltonsolo <salty-dalty@hotmail.com>
2017-05-08 19:51:10 -0600

    Created MainExecutor class

---
+++ CIT360/src/personalSandboxCode/threadsExecutablesRunnables/MainExecutor.java
@@ -0,0 +1,11 @@
+package personalSandboxCode.threadsExecutablesRunnables;
+
+/**
+ * Created by daltonsolo on 5/7/2017.
+ */
+public class MainExecutor {
+    public static void main(String[] args) {
+        // Thread pools are basically managed sets of threads, and they reduce
+        // the overhead of *thread creation*.
+    }
+}
