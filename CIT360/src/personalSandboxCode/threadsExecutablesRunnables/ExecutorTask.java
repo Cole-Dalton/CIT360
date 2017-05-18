fdbab1463ad8071b64f4a57c98f5343afe7ed731
daltonsolo <salty-dalty@hotmail.com>
2017-05-17 18:58:34 -0600

    Added more comments

--- CIT360/src/personalSandboxCode/threadsExecutablesRunnables/ExecutorTask.java
+++ CIT360/src/personalSandboxCode/threadsExecutablesRunnables/ExecutorTask.java
@@ -1,7 +1,6 @@
 package personalSandboxCode.threadsExecutablesRunnables;
 import java.io.PrintStream;
 import java.util.*;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by daltonsolo on 5/9/2017.
@@ -10,10 +9,12 @@ public class ExecutorTask implements Runnable {
     public static PrintStream p = System.out;
     private int time;
     private String name;
+    // For assigning a random sleep time.
     private Random r = new Random();
 
     public ExecutorTask(String name) {
         this.name = name;
+        // creates random sleep time for thread
         time = r.nextInt(999);
     }
 
@@ -23,12 +24,17 @@ public class ExecutorTask implements Runnable {
 
     @Override
     // Thread creation
+    // Run method from the Runnable class.
     public void run() {
+        // Prints name of the current thread
         p.println("Current thread = " + Thread.currentThread().getName());
+        // Shows when a task is being executed concurrently with another thread,
+        // then puts the thread to bed (I like saying that)
         try {
             p.println("Doing a task during : " + name);
             Thread.currentThread().sleep(time);
         }
+        // Exception for when a thread is interrupted.
         catch (InterruptedException e) {
             e.printStackTrace();
         }
